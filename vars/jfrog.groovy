31ce715822477b409b9d7a9274b7ac0165f537a3
aravindkumar00 <141475891+aravindkumar00@users.noreply.github.com>
2024-02-14 16:10:26 +0530

    Update jfrog.groovy

--- vars/jfrog.groovy
+++ vars/jfrog.groovy
@@ -1,6 +1,6 @@
 def call()
 {
   sshagent(['419e7697-10ac-47a2-be23-9e5a09ccaed3']) {
-   sh "curl -X PUT -u admin:Aravind@111 -T kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar http://13.233.136.69:8082/artifactory/libs-snapshot/"
+   sh "curl -X PUT -u admin:Aravind@111 -T target/kubernetes-configmap-reload-0.0.1-SNAPSHOT.jar http://13.233.136.69:8082/artifactory/libs-snapshot/"
   }
 }
